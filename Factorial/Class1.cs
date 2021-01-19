0ebf12c2d8491cf5fbccb1b228b9e08d0b7bb2dc
MichaelBorisov01 <bori.mix@yandex.ru>
2021-01-19 13:44:32 +0300

    Creating WPF

---
+++ Factorial/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialCalculating
+{
+    public static class Factorial
+    {
+        /// <summary>
+        /// Метод вычисления факториала
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Fact(int a)
+        {
+            int b = 1;
+            for (int i = 2; i <= a; i++)
+            {
+                
+                b*= i;
+            }
+
+            return b;
+        }
+    }
+}
---
+++ WpfAppFactorial/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using FactorialCalculating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppFactorial
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int a;
+        int result;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxNumber1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            a = Int32.Parse(TextBoxNumber.Text);
+            TextBoxResult.Text = "";
+        }
+
+        private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            result = Factorial.Fact(a);
+            TextBoxResult.Text = result.ToString();
+        }
+    }
+}
