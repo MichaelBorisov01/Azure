487780046ba70ecf2aefd6c56198abc2e3a86c20
MichaelBorisov01 <bori.mix@yandex.ru>
2021-01-21 12:12:48 +0300

    Commit

---
+++ FactorialTests/FactorialTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FactorialCalculating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialCalculating.Tests
+{
+    [TestClass()]
+    public class FactorialTests
+    {
+        [TestMethod()]
+        public void FactTest()
+        {
+            // arrange
+            int par1 = 5;
+            
+            int result = 120;
+            // act
+            var actual = Factorial.Fact(par1);
+            // assert
+            Assert.AreEqual(result, actual);
+        }
+    }
+}
